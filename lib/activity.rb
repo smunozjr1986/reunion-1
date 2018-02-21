c117a1a10366fc373ad5682ae10222ca350d349c
smunozjr1986 <sergiomunozjr07@gmail.com>
2018-02-21 09:45:10 -0700

    working on iteration one

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+  attr_reader :name,
+              :participants
+  def initialize(name)
+    @name = name
+    @participants = []
+  end
+
+  def add_participant(attendee)
+    @participants << attendee
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,30 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exist
+    activity = Activity.new("hiking")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_an_activity
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.name
+  end
+
+  def test_participants_is_empty
+    activity = Activity.new("hiking")
+    result = activity.participants
+
+    assert_equal ([]), result
+  end
+
+  def test_activity_has_participants
+    activity = Activity.new("hiking")
+    attendees_1 = activity.add_participant({"Sergio" => 20})
+
+    assert_equal attendees_1, activity.participants
+  end
 end
